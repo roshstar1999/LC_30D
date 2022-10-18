2898a14d6353361396fb24673725eb5e4c8540e1
N V Roshni <roshstar1999@gmail.com>
2022-10-18 17:14:32 +0530

    Update 38_Count_And_say_MED.py

--- 38_Count_And_say_MED.py
+++ 38_Count_And_say_MED.py
@@ -1,1 +1,25 @@
 
+class Solution:
+    def countAndSay(self,n):
+        if n==1:
+            return '1'
+    
+        l=self.countAndSay(n-1)
+    
+        x=''
+        count=1
+        i=0
+        #for i in range(len(l)-1):
+        while i<len(l)-1:
+
+            if l[i]==l[i+1]:
+                count+=1
+            else:
+                x+=str(count)+str(l[i])
+                count=1
+            i+=1
+        x+=str(count)+str(l[i])
+        return x
+
+
+        
