663fdbeb116afc81bd3a849e6e7d564d92229491
N V Roshni <roshstar1999@gmail.com>
2022-10-20 04:47:15 +0530

    Create top_k_frequent_words.py

---
+++ top_k_frequent_words.py
@@ -0,0 +1,10 @@
+
+#first time using Counter and lambda function,this was an upsolve
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        counts=collections.Counter(words)
+        items=list(counts.items())
+        items.sort(key = lambda item: (-item[1],item[0]))
+        return (item[0] for item in items[0:k])
+        
+        
